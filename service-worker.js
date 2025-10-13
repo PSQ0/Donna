a74ef5bb908c36cd6bed89e6d5826e1c3f7754bf
PSQ0 <mmg19980@gmail.com>
2025-10-13 21:30:45 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,30 @@
 const CACHE_NAME = 'birthday-trading-v1';
 const urlsToCache = [
-  './index.html',
-  './styles.css',
-  './script.js',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  '/Donna/index.html',
+  '/Donna/styles.css',
+  '/Donna/script.js',
+  '/Donna/icons/icon-192x192.png',
+  '/Donna/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
